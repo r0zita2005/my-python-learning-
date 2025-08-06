c4d3943fda42f7993eeb8e9f623ebcaddf28988d
رزیتا <rozitakereydi@gmail.com>
2025-08-06 15:34:42 +0330

    Add files via upload

---
+++ «عالی»، «قبول» یا «مردود».py
@@ -0,0 +1,9 @@
+num=int(input(" numre? "))
+if 17<=num<=20:
+    print("عالیه!!!")
+elif 10<=num<17:
+    print("قبول")
+elif num<10:
+    print("مردود")
+else :
+    print("نمره تعریف نشده")
---
+++ ۱ تا ۵ با حلقه .py
@@ -0,0 +1,7 @@
+for i in range(1 , 6):
+    print(i)
+
+j=1
+while j <=5:
+    print(j*2)
+    j+=1
